15c91d703e1496c912e0526da8200ed6ae79b939
sachin2k01 <thenamesachin@gmail.com>
2026-07-10 00:19:16 +0530

    added style for MovieCard and render inside MovieList

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import "./MovieCard.css";
+import Star_Img from "../../assets/star.png";
 
 const MovieCard = () => {
-  return <a href="#" className="movie_card">
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSedK1S4T7FINgS5uMYlsXlRB9Q9C5gFFzxMJ-Y-3pXgg&s=10" alt="Movie Poster" className="movie_poster" />
-    <h3 className="movie_title">Movie Title</h3>
-    <p className="movie_description">Short description of the movie.</p>
-  </a>;
+  return (
+    <a href="#" className="movie_card">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSedK1S4T7FINgS5uMYlsXlRB9Q9C5gFFzxMJ-Y-3pXgg&s=10"
+        alt="Movie Poster"
+        className="movie_poster"
+      />
+      <div className="movie_details">
+        <h3 className="movie_details_heading">Movie Title</h3>
+        <div className="align_center movie_date_rate">
+          <p>10-15-2023</p>
+          <p>
+            8.5
+            <img src={Star_Img} alt="rating icon" className="card_emoji" />
+          </p>
+        </div>
+        <p className="movie_description">
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </a>
+  );
 };
 
 export default MovieCard;
